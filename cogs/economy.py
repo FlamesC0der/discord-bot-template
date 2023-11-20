c7361670679e9d1fac69707c227dca9dde47db34
Alexey <alexm20games@gmail.com>
2023-11-20 03:05:17 +0300

    added stable bd

---
+++ cogs/economy.py
@@ -0,0 +1,23 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+
+class Money(commands.Cog):
+  def __init__(self, bot: commands.Bot):
+    self.bot = bot
+  
+  @app_commands.command(
+    name="set_money", description="Set Money count"
+  )
+  @app_commands.describe(user="User")
+  @app_commands.describe(amount="Amout of cash")
+  async def set_money(self, interaction: discord.Interaction, user: discord.User, amount: int = 1000):
+    result = self.bot.database.add_money(
+      user.id, interaction.guild.id, amount
+    )
+    await interaction.response.send_message(f"Set {user.name}'s money: {amount} ({result})")
+
+
+async def setup(bot: commands.Bot):
+  await bot.add_cog(Money(bot))
--- database/__init__.py
+++ database/__init__.py
@@ -5,3 +5,35 @@ class DatabaseManager():
   def __init__(self, connection: sqlite3.Connection) -> None:
     self.conn = connection
     self.cur = self.conn.cursor()
+  
+  def add_money(self, user_id: int, server_id: int, amount: int) -> int:
+    rows = self.conn.execute(
+      "SELECT id FROM economy WHERE user_id=? AND server_id=? ORDER BY id DESC LIMIT 1",
+      (
+        user_id,
+        server_id,
+      ),
+    ).fetchall()
+
+    if rows:
+      id = rows[0][0]
+      self.conn.execute(
+        "UPDATE economy SET money = (?) WHERE id = (?)",
+        (amount, id)
+      )
+    else:
+      a = self.conn.execute("SELECT * FROM economy").fetchall()
+      id = 1
+      if a:
+        id = len(a[0])
+      self.conn.execute(
+        "INSERT INTO economy(id, user_id, server_id, money) VALUES (?, ?, ?, ?)",
+        (
+          id,
+          user_id,
+          server_id,
+          amount
+        )
+      )
+    self.conn.commit()
+    return id
