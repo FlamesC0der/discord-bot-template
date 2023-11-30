0ad338a1e6063b356b4c5296612bde0231dd2c05
Alexey <alexm20games@gmail.com>
2023-11-30 22:00:41 +0300

    upd

--- cogs/music-player.py
+++ cogs/music-player.py
@@ -30,8 +30,8 @@ class Music_player(commands.Cog):
   
   @app_commands.command(name="play", description="Play music")
   @app_commands.guild_only()
-  @app_commands.describe(search="Search or link")
-  async def play(self, interaction: discord.Integration, search: str):
+  @app_commands.describe(link_or_query="Search or link")
+  async def play(self, interaction: discord.Integration, link_or_query: str):
     if not interaction.user.voice:
       return await interaction.response.send_message(embed=discord.Embed(description="You are not in a voice channel"))
     if not interaction.guild.voice_client:
@@ -39,7 +39,7 @@ class Music_player(commands.Cog):
     else:
       vc: wavelink.Player = interaction.guild.voice_client
     
-    tracks = await wavelink.Playable.search(search)
+    tracks = await wavelink.Playable.search(link_or_query)
 
     view = Music_list(tracks)
 
@@ -58,8 +58,9 @@ class Music_player(commands.Cog):
       
       return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
     
+    # Select track
     if tracks:
-      description = f"Showing results for `{search}`. Select one of result below\n"
+      description = f"Showing results for `{link_or_query}`. Select one of result below\n"
       for i in range(len(tracks)):
         if i == 5:
           break
@@ -77,8 +78,11 @@ class Music_player(commands.Cog):
 
     await view.wait()
 
-    if not vc.playing:
+    if vc.playing:
+      vc.queue.put(tracks[view.track_id])
+    else:
       await vc.play(tracks[view.track_id])
+    
     embed = discord.Embed(
       description=f"**{tracks[view.track_id].title}** has been added to the queue.",
       color=0xad1457
@@ -97,8 +101,27 @@ class Music_player(commands.Cog):
   async def pause(self, interaction: discord.Integration):
     vc: wavelink.Player = interaction.guild.voice_client
     await vc.pause(not vc.paused)
-    await interaction.response.send_message(embed=discord.Embed(description=f"The pleyer is now {vc.paused}", color=0xad1457))
+    await interaction.response.send_message(embed=discord.Embed(description=f"The pleyer is now {'paused' if vc.paused else 'unpaused'}", color=0xad1457))
   
+  @app_commands.command(name="skip", description="Skip music")
+  async def skip(self, interaction: discord.Integration):
+    vc: wavelink.Player = interaction.guild.voice_client
+    if vc:
+      if not vc.playing:
+        return await interaction.response.send_message(embed=discord.Embed(description="Nothing to skip", color=0xad1457))
+      await vc.stop()
+      if vc.paused:
+        await vc.pause(False)
+      await interaction.response.send_message(embed=discord.Embed(description="skiped current music", color=0xad1457))
+    else:
+      await interaction.response.send_message(embed=discord.Embed(description="Bot is not connected to voice channel", color=0xad1457))
+  
+  @commands.Cog.listener()
+  async def on_wavelink_track_end(self, payload: wavelink.TrackEndEventPayload):
+    if payload.player.queue:
+      next_track = payload.player.queue.get()
+      await payload.player.play(next_track)
+
   @commands.Cog.listener()
   async def on_voice_state_update(self, member, before, after):
     vc = member.guild.voice_client
@@ -109,6 +132,10 @@ class Music_player(commands.Cog):
     if len(vc.channel.members) == 1:
       await vc.stop()
       await vc.disconnect()
+  
+  @play.error
+  async def play_error(self, interaction, error):
+    await interaction.response.send_message(embed=discord.Embed(description="Failed to load song!", color=0xad1457), ephemeral=True)
 
 
 async def setup(bot: commands.Bot):
